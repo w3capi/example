a901fb8fbc83722c8023894d4e2827b4d4917a1b
w3capi <119132881+w3capi@users.noreply.github.com>
2023-08-16 01:09:47 +0700

    Initial commit

---
+++ stylelint.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  extends: [
+    'stylelint-config-standard',
+    'stylelint-config-rational-order',
+    'stylelint-config-recommended-vue',
+  ],
+  rules: {
+    'at-rule-no-unknown': [
+      true,
+      {
+        ignoreAtRules: ['tailwind', 'apply', 'variants', 'responsive', 'screen', 'layer'],
+      },
+    ],
+    'no-descending-specificity': null,
+  },
+};
